28206da7f42856408214dc2234b0dd20c5a2812b
Anton Mishchuk <antonmi@mail.ru>
2012-05-18 23:14:44 +0300

    new start strategy, examples

--- examples/one_distributor_one_node/worker.rb
+++ examples/one_distributor_one_node/worker.rb
@@ -2,5 +2,5 @@ $: << "lib"
 require 'job_reactor'
 
 JR.run! do
-  JR.start_node({:storage => JobReactor::RedisStorage, :name => "redis_node", :server => ['192.168.1.3', 6001], :distributors => [['192.168.1.3', 5001]] })
+  JR.start_node({:storage => JobReactor::RedisStorage, :name => "redis_node", :server => ['178.159.244.149', 1500], :distributors => [['192.168.1.3', 5001]] })
 end
