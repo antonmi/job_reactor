db9b55729629adaec6976d3e10542e13642a9827
Anton Mishchuk <a.mishchuk@itransition.com>
2012-05-23 21:06:07 +0300

    node server comments

--- lib/job_reactor/node/server.rb
+++ lib/job_reactor/node/server.rb
@@ -1,21 +1,27 @@
 module JobReactor
   class Node
     class Server < EM::Connection
+
+      #Need to know the storage to call save method on it
+      #Need to now node name to send it to the distributor
+      #
       def initialize(node, storage)
         @storage = storage
         @node = node
       end
 
+      #Ok, node is connected and ready to work
+      #
       def post_init
         JR::Logger.log("#{@node.name} ready to work")
       end
 
       # It is the place where job life cycle begins.
       # This method:
-      # -receive data from distributor;
-      # -save them in storage;
-      # -return 'ok' to unlock node;
-      # -and schedule job;
+      # -receives data from distributor;
+      # -saves them in storage;
+      # -returns 'ok' to unlock node;
+      # -and schedules job;
       #
       def receive_data(data)
         hash = Marshal.load(data)
@@ -28,6 +34,5 @@ module JobReactor
       end
 
     end
-
   end
 end
