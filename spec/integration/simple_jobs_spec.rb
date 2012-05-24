a7ac09337132926ae65d7be886d0aa8d3233f398
Anton Mishchuk <a.mishchuk@itransition.com>
2012-05-24 14:45:02 +0300

    readme

--- spec/integration/simple_jobs_spec.rb
+++ spec/integration/simple_jobs_spec.rb
@@ -9,17 +9,19 @@ module JobReactor
   end
 end
 
-
-JR.run do
-  JR.config[:job_directory] = File.expand_path('../../jobs', __FILE__)
-  JR.config[:retry_multiplier] = 0
-  JR.config[:max_attempt] = 5
-  JR::Distributor.start('localhost', 5000)
-  JR.start_node({:storage => JobReactor::MemoryStorage, :name => 'memory_node', :server => ['localhost', 6000], :distributors => [['localhost', 5000]] })
-end
-sleep(5)
-
 describe 'simple job' do
+
+  before(:all) do
+    JR.run do
+      JR.config[:job_directory] = File.expand_path('../../jobs', __FILE__)
+      JR.config[:retry_multiplier] = 0
+      JR.config[:max_attempt] = 5
+      JR::Distributor.start('localhost', 5000)
+      JR.start_node({:storage => JobReactor::MemoryStorage, :name => 'memory_node', :server => ['localhost', 6000], :distributors => [['localhost', 5000]] })
+    end
+    sleep(1) until EM.reactor_running?
+  end
+
   before do
     ARRAY = []
     JobReactor::MemoryStorage.flush_storage
@@ -28,7 +30,7 @@ describe 'simple job' do
   describe 'simple job' do
     it 'should do one simple job' do
       JR.enqueue 'simple', {arg1: 'arg1'}, {:node => 'memory_node'}
-      sleep(1)
+      sleep(5)
       ARRAY.size.should == 1
       ARRAY.first[0].should == 'simple'
       ARRAY.first[1].should be_instance_of(Hash)
@@ -73,7 +75,7 @@ describe 'simple job' do
     end
 
     it 'should not run "run_at" job' do
-      JR.enqueue 'simple_after', {}, {:start_at => Time.now + 1}
+      JR.enqueue 'simple_after', {}, {:run_at => Time.now + 1}
       sleep(1)
       ARRAY.size.should == 0
       sleep(5)
