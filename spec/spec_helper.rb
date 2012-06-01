c4f5b8a02ad8b5d8fb58a16567ca107fe7b4742a
Anton Mishchuk <anton.mishchuk@gmail.com>
2012-06-01 12:29:50 +0300

    require 'stringio' Gemfile.lock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,14 +2,11 @@ require 'simplecov'
 SimpleCov.start
 
 require 'job_reactor'
+
+require 'stringio'
 $logger_stream    ||= StringIO.new
 JR::Logger.stdout = $logger_stream
 
 Dir[File.expand_path('../support/*', __FILE__)].each do |file|
   require file
 end
-
-RSpec.configure do |config|
-  #config.filter_run_excluding :slow => true
-end
-
