6e24579465274cf74df1f23bf292f94f7eee6475
antonmi <antonmi@mail.ru>
2012-06-01 10:40:04 +0300

    JR config, gemspec

--- job_reactor.gemspec
+++ job_reactor.gemspec
@@ -1,16 +1,16 @@
 Gem::Specification.new do |s|
   s.name              = 'job_reactor'
-  s.version           = '0.5.0.beta1'
+  s.version           = '0.5.0.beta2'
   s.date              = Time.now.strftime('%Y-%m-%d')
   s.summary           = 'Simple, powerful and high scalable job queueing and background workers system based on EventMachine'
   s.homepage          = 'http://github.com/antonmi/job_reactor'
   s.email             = 'anton.mishchuk@gmial.com'
   s.authors           = [ "Anton Mishchuk", "Andrey Rozhkovskiy" ]
-  s.platform          = Gem::Platform::CURRENT
+  s.platform          = Gem::Platform::RUBY
 
   s.files             = Dir["lib/**/*.rb"]
   s.files             += Dir['README.markdown']
-  s.files.delete('lib/job_reactor/storages/active_record_storage.rb')
+  s.files.delete('lib/job_reactor/storages/active_record_storage.rb') #TODO next releases with rails support
 
   s.require_path     = 'lib'
 
--- lib/job_reactor/job_reactor/config.rb
+++ lib/job_reactor/job_reactor/config.rb
@@ -10,7 +10,7 @@ JR = JobReactor
 
 JR.config[:job_directory] = 'reactor_jobs'
 JR.config[:max_attempt] = 10
-JR.config[:retry_multiplier] = 2
+JR.config[:retry_multiplier] = 5
 JR.config[:retry_jobs_at_start] = true
 JR.config[:merge_job_itself_to_args] = true
 JR.config[:log_job_processing] = true
@@ -22,9 +22,10 @@ JR.config[:when_node_pull_is_empty_will_raise_exception_after] = 3600
 JR.config[:redis_host] = 'localhost'
 JR.config[:redis_port] = 6379
 
-JR.config[:active_record_adapter] = 'mysql2'
-JR.config[:active_record_database] = 'em'
-JR.config[:active_record_user] = ''
-JR.config[:active_record_password] = ''
-JR.config[:active_record_table_name] = 'reactor_jobs'
-JR.config[:use_custom_active_record_connection] = true
+#TODO next releases with rails support
+#JR.config[:active_record_adapter] = 'mysql2'
+#JR.config[:active_record_database] = 'em'
+#JR.config[:active_record_user] = ''
+#JR.config[:active_record_password] = ''
+#JR.config[:active_record_table_name] = 'reactor_jobs'
+#JR.config[:use_custom_active_record_connection] = true
