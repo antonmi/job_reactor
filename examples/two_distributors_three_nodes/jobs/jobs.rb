d157907c485e6c67cca4b49c78f8dde3e9303c10
Anton Mishchuk <anton.mishchuk@gmail.com>
2012-06-07 10:22:05 +0300

    examples

--- examples/two_distributors_three_nodes/jobs/jobs.rb
+++ examples/two_distributors_three_nodes/jobs/jobs.rb
@@ -1,14 +1,11 @@
 include JobReactor
 
-puts "="*100
-
 success = 0
 job 'success_job' do |args|
   puts 'success'
   puts success+=1
   (1..1000_000).to_a.shuffle!.sort!
   puts args[:job_itself]
-  sd
 end
 
 error = 0
