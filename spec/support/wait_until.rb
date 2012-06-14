f4654e3262007fae90b268fdef28cc82fef3eaff
joxxoxo <test@open.by>
2012-06-14 17:31:27 +0300

    wait until > period

--- spec/support/wait_until.rb
+++ spec/support/wait_until.rb
@@ -1,7 +1,9 @@
-def wait_until(max_wait_time = 5)
+def wait_until(max_wait_time = 500)
   counter = 0
-  until (block_given? && yield) || counter > max_wait_time
-    sleep(1)
+  period = 0.01
+  max_wait_counter = max_wait_time.to_f / period
+  until (block_given? && yield) || counter > max_wait_counter
+    sleep(period)
     counter = counter.succ
   end
 end
